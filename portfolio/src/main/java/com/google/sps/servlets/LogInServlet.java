878680558534eb0c1c1f5ae8583eff6a7116b095
zjw <georstra@qq.com>
2020-07-19 04:31:49 +0800

    Fix log in servlet

--- portfolio/src/main/java/com/google/sps/servlets/LogInServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LogInServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet("/data")
+@WebServlet("/login")
 public class LogInServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
